821fc4e9a2e9553e75a71eb89a832f02cded0878
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-09-03 17:02:11 +0200

    Initialize

---
+++ API/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+
+var bulider = WebApplication.CreateBuilder();
+
+var app = bulider.Build();
+
+List<string> users = new();
+
+app.MapGet("/api/users", () => users);
+app.MapPost("/api/users", (string user) => users.Add(user));
+
+app.Run();
+
