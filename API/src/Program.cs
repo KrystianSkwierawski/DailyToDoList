2c3b01ebd3fea59557336bd346f7d30d57e9ec1c
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-09-17 10:58:24 +0200

    Add Cors

--- API/src/Program.cs
+++ API/src/Program.cs
@@ -4,14 +4,32 @@ ITaskItemsDatabase _db = new TaskItemsDatabase();
 
 var bulider = WebApplication.CreateBuilder();
 
+bulider.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
 var app = bulider.Build();
+
+app.UseCors(builder => builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+);
+
 
 app.MapGet("/api/tasks", async () => await _db.GetTaskItems());
 app.MapPost("/api/tasks", async (string title) => await _db.AddTaskItemAsync(title));
 app.MapPut("/api/tasks", async (TaskItemDTO toDoItemDTO) => await _db.UpdateTaskItemAsync(toDoItemDTO));
 app.MapDelete("/api/tasks/{id}", async (string id) => await _db.DeleteTaskItemAsync(id));
 app.MapDelete("/api/tasks", async () => await _db.DeleteAllTaskItemsAsync());
-
 
 app.Run();
 
