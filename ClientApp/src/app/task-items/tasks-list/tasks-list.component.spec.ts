203b8dcebf3d0e386359a6d88cc41ee6513f61fa
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-09-29 16:27:59 +0200

    Add completeTask test and do updateOrderIndex refactor in updateOrderIndex

--- ClientApp/src/app/task-items/tasks-list/tasks-list.component.spec.ts
+++ ClientApp/src/app/task-items/tasks-list/tasks-list.component.spec.ts
@@ -1,15 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
 import { ShortenPipe } from '../../shared/pipes/shorten/shorten.pipe';
-import { appReducer, AppState } from '../../store/app.reducer';
-import { AddTaskItemLocally, DeleteTaskItemLocally, UpdateTaskItemsLocally } from '../store/task.actions';
+import * as fromApp from '../../store/app.reducer';
+import { AppState } from '../../store/app.reducer';
+import { AddTaskItemRemotely } from '../store/task.actions';
+import { TaskEffects } from '../store/task.effects';
 import { TaskItem } from '../task-item.model';
+import { TaskItemsService } from '../task-items.service';
 import { TasksListComponent } from './tasks-list.component';
+
 
 
 describe('TasksListComponent', () => {
   let store: Store<AppState>;
+  let taskItemsService: TaskItemsService;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,10 +26,13 @@ describe('TasksListComponent', () => {
         ShortenPipe
       ],
       imports: [
-        StoreModule.forRoot(appReducer),
-        BrowserAnimationsModule
+        HttpClientModule,
+        StoreModule.forRoot(fromApp.appReducer),
+        EffectsModule.forRoot([TaskEffects]),
       ]
     });
+
+    taskItemsService = TestBed.get(TaskItemsService);
 
     store = TestBed.get(Store);
     spyOn(store, 'dispatch').and.callThrough();
@@ -34,7 +45,8 @@ describe('TasksListComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('[completeTask] should complete task item locally', () => {
+  it('[completeTask] should complete task item', () => {
+    // Arrange
     const fixture = TestBed.createComponent(TasksListComponent);
     fixture.detectChanges();
     const app: TasksListComponent = fixture.componentInstance;
@@ -42,22 +54,26 @@ describe('TasksListComponent', () => {
     const taskItem: TaskItem = {
       id: "1",
       title: "test1",
-      color: "#ffff",
-      completed: true,
-      expanded: false,
-      subtaskItems: [],
-      editing: false
-    }
+    } as TaskItem;
 
-    store.dispatch(new AddTaskItemLocally(taskItem));
-    store.dispatch(new AddTaskItemLocally({ title: "test1" } as TaskItem));
+    const addTaskItemSpy = spyOn(taskItemsService, 'addTaskItem').and.returnValue(of(taskItem));
+    const deleteTaskItemSpy = spyOn(taskItemsService, 'deleteTaskItem').and.returnValue(of(taskItem.id));
 
-    store.dispatch(new DeleteTaskItemLocally(taskItem.id));
+    store.dispatch(new AddTaskItemRemotely(taskItem));
 
-    expect(app.tasks.length).toBe(1);
+
+    // Act
+    app.completeTask(taskItem);
+
+
+    // Assert
+    expect(app.tasks.length).toBe(0);
+    expect(deleteTaskItemSpy).toHaveBeenCalledTimes(1);
+    expect(addTaskItemSpy).toHaveBeenCalledTimes(1);
   });
 
   it('[updateOrderIndex] should update order index locally', () => {
+    // Arrange
     const fixture = TestBed.createComponent(TasksListComponent);
     const app: TasksListComponent = fixture.componentInstance;
     fixture.detectChanges();
@@ -83,7 +99,10 @@ describe('TasksListComponent', () => {
       },
     ];
 
+
+    // Act
     app.tasks = taskItems;
+
 
     app.updateOrderIndex({
       currentIndex: 1,
@@ -93,7 +112,8 @@ describe('TasksListComponent', () => {
     });
 
 
-    const expectedResult = taskItems.reverse();
+    // Assert
+    const expectedResult = [...taskItems].reverse();
     expect(app.tasks).toEqual(expectedResult);
   });
 
