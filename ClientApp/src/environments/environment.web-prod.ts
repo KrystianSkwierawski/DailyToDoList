f060738230e41e7b900992d1052e975b88d05a35
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-11-05 12:06:37 +0100

    Add web-production environment

--- ClientApp/src/environments/environment.web-prod.ts
+++ ClientApp/src/environments/environment.web-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: "https://localhost:44381/api/tasks",
+  production: true,
+  apiUrl: "https://dailytodolist.somee.com/api/tasks",
   webApp: true // More content will be displayed in the web application, encouraging, for example, to download the application to other devices.
 };
