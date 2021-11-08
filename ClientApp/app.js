fc60ffe3f84abb6fe56b2994a439e83402cdf388
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-11-08 18:36:10 +0100

    Add create-installer:win

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -1,6 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 let win;
 
+const setupEvents = require('./installers/setupEvents')
+if (setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
 function createWindow() {
   win = new BrowserWindow({
     width: 575,
