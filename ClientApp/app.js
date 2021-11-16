93ec066cfbb49f160f335d425207a9cc58273492
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-11-16 17:11:31 +0100

    Add icon propoerty to app.js window

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -14,7 +14,7 @@ function createWindow() {
     minWidth: 330,
     minHeight: 460,
     autoHideMenuBar: true,
-    //icon: `file://${__dirname}/dist/assets/logo.png`
+    icon: `file://${__dirname}/dist/assets/icon.ico`
   })
 
   win.loadURL(`file://${__dirname}/dist/index.html`)
