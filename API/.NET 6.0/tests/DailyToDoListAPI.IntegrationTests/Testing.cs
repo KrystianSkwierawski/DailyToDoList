756b5c39e732619627cfadf3a3b8e53d1cb081e9
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-12-01 15:11:14 +0100

    Add a different connection string to the test database
    GitHub ci action was doing tests API with .net 6.0 and .net core 3.1 at the same time# Please enter the commit message for your changes. Lines starting

--- API/.NET 6.0/tests/DailyToDoListAPI.IntegrationTests/Testing.cs
+++ API/.NET 6.0/tests/DailyToDoListAPI.IntegrationTests/Testing.cs
@@ -5,7 +5,7 @@ namespace DailyToDoListAPI.IntegrationTests
 {
     public class Testing
     {
-        public static ITaskItemsDatabase GetTestDatabase(string connectionString = "mongodb+srv://test:123@cluster0.nrv7f.mongodb.net/DailyToDoListAPITestDB?retryWrites=true&w=majority")
+        public static ITaskItemsDatabase GetTestDatabase(string connectionString = "mongodb+srv://test:123@cluster0.nrv7f.mongodb.net/DailyToDoListAPI_dotnet6.0TestDB?retryWrites=true&w=majority")
         {
             return new TaskItemsDatabase(connectionString);
         }
