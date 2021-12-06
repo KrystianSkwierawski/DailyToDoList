0fc8ab57197721c081d9ee79a92f44d3878cc483
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-12-06 15:05:33 +0100

    Add validator to authentication-form

--- ClientApp/src/app/authentication/authentication-form/authentication-form.component.ts
+++ ClientApp/src/app/authentication/authentication-form/authentication-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.reducer';
 import { SetToken, SetTokenInLocalStorage } from '../store/authentication.actions';
@@ -17,7 +17,11 @@ export class AuthenticationFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      token: new FormControl('')
+      token: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25)
+      ])
     });
   }
 
@@ -26,5 +30,23 @@ export class AuthenticationFormComponent implements OnInit {
       this.store.dispatch(new SetTokenInLocalStorage(token));
 
     this.store.dispatch(new SetToken(token));
+  }
+
+  getErrorMessageFieldToken() {
+    const field = this.form.get('token');
+
+    if (field?.hasError('required')) {
+      return 'The token is required';
+    }
+
+    if (field?.hasError('maxlength')) {
+      return 'The maximum token length is 25';
+    }
+
+    if (field?.hasError('minlength')) {
+      return 'The minimum token length is 3';
+    }
+
+    return '';
   }
 }
