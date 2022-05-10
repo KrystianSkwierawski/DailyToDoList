ab63f296dd8c07d2216a01f640350481c753fb23
Krystian Skwierawski <bezi25251@wp.pl>
2022-05-10 14:39:33 +0200

    Update prod api url

--- ClientApp/src/environments/environment.web-prod.ts
+++ ClientApp/src/environments/environment.web-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://dailytodolist.somee.com/api/tasks",
+  apiUrl: "https://dailytodolistv2.somee.com/api/tasks",
   webApp: true // More content will be displayed in the web application, encouraging, for example, to download the application to other devices.
 };
