a4c20cbe23ec78fa26c7bd6b3b6a39e53c96a572
Krystian Skwierawski <52860350+KrystianSkwierawski@users.noreply.github.com>
2024-07-01 18:28:58 +0200

    uodate

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://dailytodolistv2.somee.com/api/tasks",
-  webApp: false // More content will be displayed in the web application, encouraging, for example, to download the application to other devices.
+  apiUrl: "https://dailytodolist.bsite.net/api/tasks",
+  webApp: true // More content will be displayed in the web application, encouraging, for example, to download the application to other devices.
 };
